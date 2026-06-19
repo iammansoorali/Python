58aaf2120571921f3ef859905eb4f4dba03d826a
Mansoor <skmakp360@gmail.com>
2026-06-19 21:54:12 +0530

    Assignment1 and Assignment 2

---
+++ Assignment1.py
@@ -0,0 +1,4 @@
+#print your name 10 times using *
+name=input("Enter your name:\n")
+print((name + '\n') * 10)
+print(id(name))
---
+++ Assignment2.py
@@ -0,0 +1,14 @@
+#Assignment 1: Print name 10 times using *
+#print("Name\n" * 10)
+
+#CodeExecution
+print("Line 1")
+print("Line 2")
+print("Line 3")
+      
+#Assignment take diameter as input and calcualte area of a circle
+dia = int(input("Enter the diameter of the circle:\n"))
+Area= round((3.14159 * dia * dia)/4,3)
+print(("Area of the circle with"),dia, ("as diameter is"), Area)
+print(type(Area))
+
