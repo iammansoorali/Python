7c3396885a6a65e6c4a00dda698a59a7ca4fd32f
Mansoor <skmakp360@gmail.com>
2026-06-19 21:55:18 +0530

    Assignment3

---
+++ Assignment3.py
@@ -0,0 +1,7 @@
+#Input two numbers and print sum
+input1=int(input("Enter first number:"))
+input2=int(input("Enter second number:"))
+sum = input1 + input2
+print("The sum is:", sum)
+average = (input1 + input2) / 2
+print("The average is:", average)
